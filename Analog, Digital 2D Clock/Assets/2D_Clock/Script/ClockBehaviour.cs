b9510390705346b34a2375f3eefcf0515953ff5c
Oetoekkae <aleksi.kouhi@edu.turkuamk.fi>
2020-03-31 14:10:16 +0300

    2D Clock

---
+++ Analog, Digital 2D Clock/Assets/2D_Clock/Script/ClockBehaviour.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+
+public class ClockBehaviour : MonoBehaviour {
+    [Header("Clock Arm")]
+    public Transform hoursAnalog, minutesAnalog, secondsAnalog;
+    [Header("Digital Values")]
+    public TextMeshProUGUI hoursDigi, minutesDigi, secondsDigi;
+
+    private const float hours = -360f / 12f,
+        minutes = -360f / 60f,
+        seconds = -360f / 60f;
+
+    // Update is called once per framed
+    void Update() { 
+        updateTime();
+    }
+
+    //Päivittää kellon ajan
+    void updateTime() {
+
+        //Tämän hetkinen aika
+        DateTime currentTime = DateTime.Now;
+
+        //Viisareiden asetus oikeaan kulmaan ajan perusteella
+        hoursAnalog.localRotation = Quaternion.Euler(0f, 0f, (float)currentTime.Hour * hours + ((float)currentTime.Minute * minutes * (float)0.055));
+        minutesAnalog.localRotation = Quaternion.Euler(0f, 0f, (float)currentTime.Minute * minutes);
+        secondsAnalog.localRotation = Quaternion.Euler(0f, 0f, (float)currentTime.Second * seconds);
+
+        //Digitaalisen ajan asetus
+        hoursDigi.SetText(checkForSingleDigit(currentTime.Hour));
+        minutesDigi.SetText(checkForSingleDigit(currentTime.Minute));
+        secondsDigi.SetText(checkForSingleDigit(currentTime.Second));
+    }
+
+    //Tarkastaa mikäli arvo on yksinumeroinen ja palauttaa sen kaksinumeroisena merkkijonona
+    String checkForSingleDigit(int digits) {
+        if (digits < 10) {
+            string addZero = "0" + digits.ToString();
+            return addZero;
+        }
+        return digits.ToString();
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Unity-2D-Clock
- Simple 2d clock with analog and digital intefaces made with Unity 3D
+ Simple 2d clock with analog and digital intefaces made with Unity 3D.
+ Code explanation is in pdf file '2dKellonToiminta.pdf' in Finnish.
